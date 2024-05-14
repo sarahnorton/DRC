a553a1e3a523b31aeb35ad95689288222c6cafde
sarahnorton <sarah.norton04@gmail.com>
2024-05-14 20:39:26 +1000

    Realsense module stuff

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # the start of a fantastic DRC robot
+
 print("Hello World")
 
 # curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 # python get-pip.py
 # pip install opencv-python
 # pip install imutils
+# pip install pyrealsense2
