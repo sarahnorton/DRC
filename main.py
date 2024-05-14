012ebd13d9251b83e8fef98d48f48fae8adae26c
sarahnorton <sarah.norton04@gmail.com>
2024-05-14 20:09:57 +1000

    pipppp

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # the start of a fantastic DRC robot
 print("Hello World")
 
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python get-pip.py
 # pip install opencv-python
 # pip install imutils
